294c4ef21f9ce4268fda77113067fa250b030608
Kamila Vit�ria de Souza Cardozo <56631494818@>
2025-05-22 11:25:36 -0300

    adicionei algumas alteracoes com a prof fer

--- formativa/projeto/settings.py
+++ formativa/projeto/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'app',
-    'corsheader',
+    'corsheaders',
 ]
 #reconhecer o projeto
 AUTH_USER_MODEL = 'app.Usuario'
